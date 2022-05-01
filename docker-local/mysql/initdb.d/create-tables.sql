e53976f8937624a0f9e8587c4b80355bcc139470
tomatokey <tomatokey.contact@gmail.com>
2022-05-01 11:56:12 +0900

    [mod] 更新と参照用のDBアカウントを作成

--- docker-local/mysql/initdb.d/create-tables.sql
+++ docker-local/mysql/initdb.d/create-tables.sql
@@ -1,4 +1,6 @@
-CREATE USER 'user_ref'@'%' IDENTIFIED BY 'password';
+CREATE USER 'user_upd'@'%' IDENTIFIED BY 'user_upd_password';
+GRANT ALL ON local_db.* TO 'user_upd'@'%';
+CREATE USER 'user_ref'@'%' IDENTIFIED BY 'user_ref_password';
 GRANT SELECT ON local_db.* TO 'user_ref'@'%';
 
 drop table if exists user_t;
