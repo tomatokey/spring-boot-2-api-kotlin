48605bfe66eb0a15255f9312c4c74e112d26f28e
tomatokey <tomatokey.contact@gmail.com>
2022-05-04 11:28:42 +0900

    [del] 不要な処理を削除

--- src/main/java/com/tomatokey/architecture/layer_03_domain/ValueObject.java
+++ src/main/java/com/tomatokey/architecture/layer_03_domain/ValueObject.java
@@ -4,9 +4,12 @@ import com.fasterxml.jackson.annotation.JsonCreator;
 import com.fasterxml.jackson.annotation.JsonValue;
 import com.tomatokey.architecture.layer_03_domain.user.UserId;
 
-import java.io.Serializable;
-
-public abstract class ValueObject<T extends Comparable<T>> implements Serializable {
+/**
+ * 値オブジェクト用の基底クラス
+ *
+ * @param <T> value
+ */
+public abstract class ValueObject<T extends Comparable<T>> {
 
     /**
      * {@link JsonValue}を付与することによって、例えば
@@ -21,7 +24,7 @@ public abstract class ValueObject<T extends Comparable<T>> implements Serializab
      * {@code {"user_id": 1}}のJsonが
      * {@link UserId}オブジェクトにデシリアライズされます
      */
-    @JsonCreator(mode = JsonCreator.Mode.DELEGATING)
+    @JsonCreator
     protected ValueObject(T value) {
         this.value = value;
     }
