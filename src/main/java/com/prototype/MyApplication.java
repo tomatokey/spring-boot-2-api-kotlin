d3e04111a91169401754c39126e85b9db3ec831c
tomatokey <tomatokey.contact@gmail.com>
2022-05-17 09:39:21 +0900

    [add] war生成処理を追加

--- src/main/java/com/prototype/MyApplication.java
+++ src/main/java/com/prototype/MyApplication.java
@@ -2,9 +2,21 @@ package com.prototype;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.boot.builder.SpringApplicationBuilder;
+import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 
+/**
+ * SpringBootアプリケーションクラス
+ *
+ * @see <a href="https://spring.pleiades.io/spring-boot/docs/current/reference/html/howto.html#howto.traditional-deployment.war">デプロイ可能な War ファイルを作成する</a>
+ */
 @SpringBootApplication
-public class MyApplication {
+public class MyApplication extends SpringBootServletInitializer {
+
+	@Override
+	protected SpringApplicationBuilder configure(SpringApplicationBuilder application) {
+		return application.sources(MyApplication.class);
+	}
 
 	public static void main(String[] args) {
 		SpringApplication.run(MyApplication.class, args);
