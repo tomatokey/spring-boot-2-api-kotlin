52758d02d6e3947690dec7b55e1464130e6df575
tomatokey <tomatokey.contact@gmail.com>
2022-06-12 11:33:10 +0900

    [mod] kotlinのリフレクションを用いるように変更

--- src/main/kotlin/com/prototype/framework/extension/AnyExtension.kt
+++ src/main/kotlin/com/prototype/framework/extension/AnyExtension.kt
@@ -5,32 +5,35 @@ import org.slf4j.Logger
 import org.slf4j.LoggerFactory
 import org.springframework.core.convert.TypeDescriptor
 import java.lang.reflect.ParameterizedType
+import kotlin.reflect.full.primaryConstructor
 
 val Any.log: Logger
     get() = LoggerFactory.getLogger(this.javaClass)
 
 fun Any.toSingleValueObject(sourceType: TypeDescriptor, targetType: TypeDescriptor): Any {
-    val targetGenericType = targetType.type.genericInterfaces
-            .map { it as? ParameterizedType }
-            .firstOrNull { it?.rawType == SingleValueObject::class.java }
-            ?.actualTypeArguments?.getOrNull(0)
+    val targetKClass = targetType.type.kotlin
+    val targetGenericType = targetKClass.supertypes
+            .firstOrNull { it.classifier == SingleValueObject::class }
+            ?.arguments?.getOrNull(0)?.type?.classifier
+            ?: throw IllegalStateException("${targetKClass}はSingleValueObjectを継承していません")
+    val targetConstructor = targetKClass.primaryConstructor ?: throw IllegalStateException("${targetKClass}にコンストラクタが存在しません")
 
     if (this is String) {
         return when (targetGenericType) {
-            String::class.javaObjectType -> targetType.type.getConstructor(String::class.java).newInstance(this)
-            Int::class.javaObjectType -> targetType.type.getConstructor(Int::class.java).newInstance(this.toInt())
-            Long::class.javaObjectType -> targetType.type.getConstructor(Long::class.java).newInstance(this.toLong())
-            Float::class.javaObjectType -> targetType.type.getConstructor(Float::class.java).newInstance(this.toFloat())
-            Double::class.javaObjectType -> targetType.type.getConstructor(Double::class.java).newInstance(this.toDouble())
+            String::class -> targetConstructor.call(this)
+            Int::class -> targetConstructor.call(this.toInt())
+            Long::class -> targetConstructor.call(this.toLong())
+            Float::class -> targetConstructor.call(this.toFloat())
+            Double::class -> targetConstructor.call(this.toDouble())
             else -> throw IllegalStateException("String型から${targetGenericType}への変換処理を追加してください")
         }
     }
     if (this is Number) {
         return when (targetGenericType) {
-            Int::class.javaObjectType -> targetType.type.getConstructor(Int::class.java).newInstance(this.toInt())
-            Long::class.javaObjectType -> targetType.type.getConstructor(Long::class.java).newInstance(this.toLong())
-            Float::class.javaObjectType -> targetType.type.getConstructor(Float::class.java).newInstance(this.toFloat())
-            Double::class.javaObjectType -> targetType.type.getConstructor(Double::class.java).newInstance(this.toDouble())
+            Int::class -> targetConstructor.call(this.toInt())
+            Long::class -> targetConstructor.call(this.toLong())
+            Float::class -> targetConstructor.call(this.toFloat())
+            Double::class -> targetConstructor.call(this.toDouble())
             else -> throw IllegalStateException("Number型から${targetGenericType}への変換処理を追加してください")
         }
     }
